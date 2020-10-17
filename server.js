af12c6506beb5fbbb2f0e6344651e6bbb1a6b0c2
rolandomh <ro@spacelabforever.com>
2020-10-17 16:55:05 -0700

    city-explorer

--- server.js
+++ server.js
@@ -26,7 +26,19 @@ app.get('/location', (request, response) => {
   // getting the data from a database or API, using a flat file
   let data = require('./data/location.json')[0];
   let location = new Location(data, city);
+  console.log (location);
   response.send(location);
+});
+app.get('/weather', (request, response) => {
+  let data = require('./data/weather.json');
+  let weatherArray = [];
+  data.data.forEach(value => {
+    let weather = new Weather(value);
+    weatherArray.push(weather);
+  })
+  console.log(weatherArray);
+  response.send(weatherArray);
+
 });
 
 app.get('/restaurants', (request, response) => {
@@ -41,13 +53,20 @@ app.get('/restaurants', (request, response) => {
 
 });
 
+
 // Constructor to tailor our incoming raw data
 
 function Location(obj, query){
-  this.lat = obj.lat;
-  this.lon = obj.lon;
+  this.latitude = obj.lat;
+  this.longitude = obj.lon;
   this.search_query = query;
-  this.location = obj.display_name;
+  this.formatted_query = obj.display_name;
+}
+// Constructor function for weather
+
+function Weather(obj){
+  this.forecast = obj.weather.description;
+  this.time = new Date( obj.ts*1000).toDateString();
 }
 
 function Restaurant(obj) {
@@ -59,13 +78,16 @@ function Restaurant(obj) {
 }
 
 
-// Start our server! 
+// Start our server!
 function startServer() {
-   app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`)
-   });
+  });
 }
 
-client.connect()
-.then(startServer)
-.catch(e => console.log(e));
+startServer()
+
+//postgres
+// client.connect()
+//   .then(startServer)
+//   .catch(e => console.log(e));

159fd0754f68a0467720427bf4a8c2cda68a1833
rolandomh <ro@spacelabforever.com>
2020-11-08 20:34:30 -0800

    something is being called out of order

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const express = require('express');
 const superagent = require('superagent');
 const cors = require('cors');
 const pg = require('pg');
+const { request } = require('express');
 const client = new pg.Client(process.env.DATABASE_URL);
-const PORT = process.env.PORT || 3001;
-// ERROR CALL
-client.on('error', err => console.error(err));
 // Application Setup
 const PORT = process.env.PORT || 3000;
+// ERROR CALL
+client.on('error', err => console.error(err));
 // Start express
 const app = express();
 // use CORS
@@ -28,35 +28,35 @@ app.get('/', (req, res) => {
 res.send('Hello World');
 });
 //ERROR CALLBACK
-const errorAlert = (err, res) => {
-res.status(500).send('NOPE');
+const errorAlert = (err, response) => {
+response.status(500).send('NOPE');
 console.log('error', err);
 }
 // Location Function
-function locationHandler(req, res) {
-let city = req.query.city;
+function locationHandler(req, response) {
+let city = request.query.city;
 let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEO_DATA_API_KEY}&q=${city}&format=json`;
 let sql = 'SELECT * FROM locations WHERE search_query LIKE ($1);'
 let safeValue = [city];
 client.query(sql, safeValue)
 .then(sqlResults => {
 if (sqlResults.rowCount) {
-res.status(200).send(sqlResults.rows[0]);
+response.status(200).send(sqlResults.rows[0]);
 } else {
 superagent.get(url)
 .then(results => {
 let finalObj = new Location(city, results.body[0]);
 let sql = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);';
 let safeValue = [finalObj.search_query, finalObj.formatted_query, finalObj.latitude, finalObj.longitude];
-res.status(200).send(finalObj);
+response.status(200).send(finalObj);
 client.query(sql, safeValue);
-}).catch(error => errorAlert(error, res));
+}).catch(error => errorAlert(error, response));
 }
-}).catch(error => errorAlert(error, res));
+}).catch(error => errorAlert(error, response));
 }
 // Weather Function
-function weatherHandler(req, res) {
-let search_query = req.query.search_query;
+function weatherHandler(request, response) {
+let search_query = request.query.search_query;
 let url = 'https://api.weatherbit.io/v2.0/forecast/daily';
 const queryParams = {
 city: search_query,
@@ -66,16 +66,16 @@ days: 8
 superagent.get(url)
 .query(queryParams)
 .then(results => {
-let weatherResults = res.body.data.map(weatherResults => {
+let weatherResults = response.body.data.map(weatherResults => {
 let day = new Weather(weatherResults);
 return day;
 });
-res.status(200).send(weatherResults);
+response.status(200).send(weatherResults);
 }).catch(error => errorAlert(error, res));
 }
 // Yelp Function
-function yelpHandler(req, res) {
-let city = req.query.search_query;
+function yelpHandler(request, response) {
+let city = request.query.search_query;
 let url = 'https://api.yelp.com/v3/businesses/search';
 const queryParams = {
 location: city,
@@ -89,11 +89,11 @@ superagent.get(url)
 let foodData = data.body.businesses;
 let allRestuarants = foodData.map(food => new Restaurant(food));
 // console.log('results from superagent', data.body);
-res.status(200).send(allRestuarants);
+response.status(200).send(allRestuarants);
 }).catch(error => errorAlert(error, response));
 }
 // Movie Handler
-function movieHandler(req, res) {
+function movieHandler(request, response) {
 let search_query = req.query.search_query;
 let url = 'https://api.weatherbit.io/v2.0/forecast/daily';
 const queryParams = {
@@ -108,7 +108,7 @@ let weatherResults = results.body.data.map(weatherResults => {
 let day = new Weather(weatherResults);
 return day;
 });
-res.status(200).send(weatherResults);
+response.status(200).send(weatherResults);
 }).catch(error => errorAlert(error, response));
 
 // Trails Handler
@@ -158,22 +158,15 @@ this.image_url = `https://image.tmdb.org/t/p/w500/${obj.poster_path}`;
 this.popularity = obj.popularity;
 this.released_on = obj.release_date;
 }
-
-
-// Start our server!
+// postgres
+// .then startServer
 function startServer() {
+// .catch(e => console.log(e));
 app.listen(PORT, () => {
 console.log(`App is listening on ${PORT}`)
 });
 }
-
-startServer()
-
-//postgres
 // client.connect()
-// .then(startServer)
-// .catch(e => console.log(e));
-
 // Make sure the server is listening for reqs
 client.connect()
 .then(() => {
